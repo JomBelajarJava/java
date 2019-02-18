b655f88064faa7e31c956bda9c7d98d780765a45
Burhanuddin Baharuddin <burhanclj@gmail.com>
2019-02-19 05:34:37 +0800

    Update adsense plugin

---
+++ docs/gitbook/gitbook-plugin-adsense/adsense.js
@@ -0,0 +1,8 @@
+require(['gitbook'], function(gitbook) {
+    gitbook.events.on('page.change', function() {
+        (adsbygoogle = window.adsbygoogle || []).push({
+            google_ad_client: "ca-pub-8041854815282465",
+            enable_page_level_ads: true
+        });
+    });
+});
